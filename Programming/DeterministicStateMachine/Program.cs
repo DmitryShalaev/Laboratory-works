2b6b25e4b9112e172dbcb8f1f8c2326311fc26e5
Dmitry <62142602+DmitryShalaev@users.noreply.github.com>
2022-03-23 21:13:04 +0300

    update

--- Programming/DeterministicStateMachine/Program.cs
+++ Programming/DeterministicStateMachine/Program.cs
@@ -1,5 +1,5 @@
 ﻿class DSM {
-	class Key {
+	private struct Key {
 		public Key(string state, char transition) {
 			State = state;
 			Transition = transition;
@@ -7,24 +7,24 @@
 
 		public string State { get; private set; }
 		public char Transition { get; private set; }
-
-		public override bool Equals(object? obj) => obj is Key key && State == key.State && Transition == key.Transition;
-		public override int GetHashCode() => HashCode.Combine(State, Transition);
 	}
 
 	private readonly Dictionary<Key, string> _states = new() {
-		{new("S",'0'), "Z" },
-		{new("S",'1'), "V" },
-		{new("V",'0'), "Z" },
-		{new("V",'1'), "V" },
-		{new("Z",'0'), "Z" },
-		{new("Z",'1'), "V" }
+		{new("S",'0'), "Z"},
+		{new("S",'1'), "V"},
+		{new("V",'0'), "Z"},
+		{new("V",'1'), "V"},
+		{new("Z",'0'), "Z"},
+		{new("Z",'1'), "V"}
 	};
 
-	public bool Run(string str) => Run("S", str);
+	private readonly string _startState = "S";
+	private readonly string _endState = "Z";
+
+	public bool Run(string str) => Run(_startState, str);
 
 	private bool Run(string state, string str) {
-		if(str.Length == 0 && state == "Z") return true;
+		if(str.Length == 0 && state == _endState) return true;
 		if(str.Length == 0) return false;
 
 		try {
@@ -41,6 +41,6 @@
 class Program {
 	static void Main() {
 		DSM dSM = new();
-		Console.WriteLine(dSM.Run("100"));
+		Console.WriteLine(dSM.Run("10010"));
 	}
 }
