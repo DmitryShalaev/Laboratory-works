419076a0878a8a858ed28d27ef01579596851c5c
Dmitry <62142602+DmitryShalaev@users.noreply.github.com>
2022-03-23 19:56:07 +0300

    Add Programming

---
+++ Programming/DeterministicStateMachine/Program.cs
@@ -0,0 +1,46 @@
+﻿class DSM {
+	class Key {
+		public Key(string state, char transition) {
+			State = state;
+			Transition = transition;
+		}
+
+		public string State { get; private set; }
+		public char Transition { get; private set; }
+
+		public override bool Equals(object? obj) => obj is Key key && State == key.State && Transition == key.Transition;
+		public override int GetHashCode() => HashCode.Combine(State, Transition);
+	}
+
+	private readonly Dictionary<Key, string> _states = new() {
+		{new("S",'0'), "Z" },
+		{new("S",'1'), "V" },
+		{new("V",'0'), "Z" },
+		{new("V",'1'), "V" },
+		{new("Z",'0'), "Z" },
+		{new("Z",'1'), "V" }
+	};
+
+	public bool Run(string str) => Run("S", str);
+
+	private bool Run(string state, string str) {
+		if(str.Length == 0 && state == "Z") return true;
+		if(str.Length == 0) return false;
+
+		try {
+			string _nextState = _states[new(state, str[0])];
+			Console.WriteLine($"({state},{str[0]})->{_nextState}");
+			return Run(_nextState, str.Remove(0, 1));
+		} catch(KeyNotFoundException) {
+			Console.WriteLine($"State \"{str[0]}\" not found");
+			return false;
+		}
+	}
+}
+
+class Program {
+	static void Main() {
+		DSM dSM = new();
+		Console.WriteLine(dSM.Run("100"));
+	}
+}
---
+++ Programming/Dictionary/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Program;
+
+class Pair {
+	public string str;
+	public char chr;
+
+	public Pair(string str, char chr) {
+		this.str = str;
+		this.chr = chr;
+	}
+
+	public override bool Equals(object? obj) {
+		if(obj is not Pair pair) return false;
+
+		return str.Equals(pair.str) && chr.Equals(pair.chr);
+	}
+
+	public override string ToString() { 
+		return $"(\"{str}\",\'{chr}\')";
+	}
+
+	public override int GetHashCode() {
+		return str.GetHashCode() + chr.GetHashCode();
+	}
+}
+
+class Program {
+	static void Main() { 
+	
+		Dictionary<Pair, string> pairs = new();
+		pairs.Add(new("A",'a'), "S");
+		pairs.Add(new("A",'b'), "Hello");
+		pairs.Add(new("A",'c'), "World");
+
+		Console.WriteLine(pairs[new("A", 'b')]);
+
+		foreach(var item in pairs) {
+			Console.WriteLine(item.Key +" -> \"" + item.Value+"\"");
+		}
+	}
+}
