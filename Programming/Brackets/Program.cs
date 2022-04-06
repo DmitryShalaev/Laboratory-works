b71ddcaf674c2623dfef4382f69047b6b710a4b3
Dmitry <62142602+DmitryShalaev@users.noreply.github.com>
2022-04-06 18:53:28 +0300

    Add Brackets

---
+++ Programming/Brackets/Program.cs
@@ -0,0 +1,24 @@
+﻿static class Program {
+
+	static bool isBalanced(string str) {
+		Stack<char> stack = new();
+
+		foreach(char ch in str) {
+			if(ch == '(')
+				stack.Push(ch);
+			else if(ch == ')') {
+				if(stack.Count == 0)
+					return false;
+				
+				stack.Pop();
+			}
+		}
+		return stack.Count == 0;
+	}
+
+	static void Main() {
+		string str = "((())(9+2)()())";
+
+		Console.WriteLine(isBalanced(str));
+	}
+}
